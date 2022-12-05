d64a0ab9783b17a8ac45f9405c7d4bf38bdf4a7e
Haruka　Karakawa <85378204+427harinn@users.noreply.github.com>
2022-12-05 18:54:37 +0900

    店舗一覧のデザインを変更、SQL文追加

--- pbl2022/src/rst_list.php
+++ pbl2022/src/rst_list.php
@@ -8,25 +8,24 @@
   $rs = $conn->query($sql);
   if (!$rs) die('エラー: ' . $conn->error);
   ?>
-  <table>
-    <tr>
-      <?php
-      //架空の店舗ID（1～3）を使って一覧を作っている
-      //実際の店舗情報は、DBから検索したものを使ってください
-      //DBからデータを検索し、while文で複数の店舗の情報を一つずつ受けとり、画像とどもに表示する
-      $row= $rs->fetch_assoc();
-      while($row) {
-        echo '<td>';
-        echo '<a href="?do=rst_detail&rst_id=' .$row['rst_id'] . '">';
-        echo '<img src="img/rst' . $row['rst_id'] . '_photo1.jpg" height="180">';
-        echo '</a>';
-        echo '<br>', '店舗名：' .$row['rst_name'] ;
-        echo '<br>', '評価数：' .$row['count'] ;
-        echo '<br>', '評価平均：' .$row['ave'] ;
-        echo '<br>', 'ジャンル：' .$row['genre'] ;
-        $row= $rs->fetch_assoc();//次の行へ
-      }
-      ?>
-    </tr>
-  </table>
+  <?php
+  echo '<div class = "rst_grid">';
+  //架空の店舗ID（1～3）を使って一覧を作っている
+  //実際の店舗情報は、DBから検索したものを使ってください
+  //DBからデータを検索し、while文で複数の店舗の情報を一つずつ受けとり、画像とどもに表示する
+  $row = $rs->fetch_assoc();
+  while ($row) {
+    echo '<div class = "item">';
+    echo '<a href="?do=rst_detail&rst_id=' . $row['rst_id'] . '">';
+    echo '<img src="img/rst1_photo1.jpg">';
+    echo '</a>';
+    echo '<br>', '店舗名：' . $row['rst_name'];
+    echo '<br>', '評価数：' . $row['count'];
+    echo '<br>', '評価平均：' . $row['ave'];
+    echo '<br>', 'ジャンル：' . $row['genre'];
+    echo '</div>';
+    $row = $rs->fetch_assoc(); //次の行へ
+  }
+  echo '</div>';
+  ?>
 </div>
