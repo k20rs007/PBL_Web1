41017aec0e8c13b1937957d3fe1b6c98a5d2780c
Haruka　Karakawa <85378204+427harinn@users.noreply.github.com>
2022-12-10 17:40:46 +0900

    マイページに自分が投稿した物を表示

--- pbl2022/src/pg_mypage.php
+++ pbl2022/src/pg_mypage.php
@@ -19,6 +19,68 @@
           echo '<br>';
           echo '<br>';
           echo '<h2>投稿した店舗一覧</h2>';
+  
+          //投稿した店舗一覧
+          require_once('db_inc.php');
+          define('MAX_ROWS', 9); //MAX_ROWS: 1ページに表示する最大行数
+          
+          $sql = "SELECT *, (SELECT COUNT(*) FROM t_review WHERE t_rstinfo.rst_id = t_review.rst_id) as count,
+          (SELECT ROUND(AVG(eval_point),1) FROM t_review WHERE t_rstinfo.rst_id = t_review.rst_id) as ave 
+          FROM t_rstinfo JOIN t_open ON t_rstinfo.rst_id = t_open.rst_id JOIN t_genre ON t_rstinfo.rst_id = t_genre.rst_id WHERE user_id = '{$uid}'";
+
+          $rs = $conn->query($sql);
+          $num_rows = mysqli_num_rows($rs);
+          $max_page = ceil($num_rows / MAX_ROWS);
+          if (!$rs) die('エラー: ' . $conn->error);
+        
+          //表示したいページの番号、行番号を求める
+          $page = isset($_GET['p']) ? $_GET['p'] : 1;
+          if ($page < 1) $page = 1; //無効なページ番号を回避
+          if ($page > $max_page) $page = $max_page;
+          $offset = ($page - 1)  * MAX_ROWS;
+        
+        
+        
+        
+          echo '<div class = "rst_grid">';
+
+          $sql = sprintf("%s LIMIT %d OFFSET %d", $sql, MAX_ROWS, $offset);
+          $rs = $conn->query($sql);
+          if ($rs) {
+            $row = $rs->fetch_assoc();
+            while ($row) {
+              echo '<div class = "item">';
+              echo '<a href="?do=rst_detail&rst_id=' . $row['rst_id'] . '">';
+              echo '<img src="img/rst1_photo1.jpg">';
+              echo '</a>';
+              if ($row['takeout'] == 1) {
+                echo '<br>', 'テイクアウト：可能';
+              } else {
+                echo '<br>', 'テイクアウト：不可';
+              }
+              if ($row['delivery'] == 1) {
+                echo '　', 'デリバリー：可能';
+              } else {
+                echo '　', 'デリバリー：不可';
+              }
+              echo '<br>', '店舗名：' . $row['rst_name'];
+              echo '<br>', '評価数：' . $row['count'];
+              echo '<br>', '評価平均：' . $row['ave'];
+              echo '<br>', 'ジャンル：' . $row['genre'];
+              echo '<br>', '平均予算：' . ($row['budget_max'] + $row['budget_min']) / 2 . '円';
+              echo '</div>';
+              $row = $rs->fetch_assoc(); //次の行へ
+            }
+          }
+        
+          echo '</div>';
+          echo '<div class = "paging">';
+          for ($j = 1; $j <= $max_page; $j++) {
+            if ($j == $page) echo $j, ' ';
+            else printf('<a href="?do=pg_mypage&p=%d">%d</a> ', $j, $j);
+          }
+          echo '</div>';
+          
         }
         if ($_SESSION['usertype_id']==='9'){  //管理者
           $url1 = "location.href='./?do=usr_add&user_id=".$row['user_id']."'";
